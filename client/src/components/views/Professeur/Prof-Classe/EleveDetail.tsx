62b4220468b25690e5efce69271260526fc53d96
Adam01-i <seckmote@gmail.com>
2025-07-06 03:32:24 +0000

    main

--- client/src/components/views/Professeur/Prof-Classe/EleveDetail.tsx
+++ client/src/components/views/Professeur/Prof-Classe/EleveDetail.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { supabase } from '../../../../lib/supabaseClient';
 import { EleveActivite } from '../../../../types/Eleve/EleveActivite';
